c2e782e4b088415787d252fa566852a4ae3c22f7
Sergej Sawazki <ce3a@gmx.de>
2013-12-27 20:35:46 +0100

    working on config xml file...

--- src/Configuration.cs
+++ src/Configuration.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Xml.Linq;
 
 namespace indicatorstocks
 {
@@ -8,28 +9,49 @@ namespace indicatorstocks
 	{
 		public static readonly Configuration Instance = new Configuration();
 
-		private string path;
+		private readonly string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+		private readonly string fileName = "conf.xml";
+
+		private string filePath;
+
+		private XDocument confDoc;
 
 		private Configuration(){}
 
-		public void Init(string name)
+		public void Init(string appName)
 		{
-			path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData); 
-			path += @"/" + name + @"/symbols.conf";
+			oldPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData); 
+			oldPath += @"/" + appName + @"/symbols.conf";
 		
-			if (!Directory.Exists (Path.GetDirectoryName (path)))
-				Directory.CreateDirectory (Path.GetDirectoryName (path));
+			// new concept..
+			filePath += Path.Combine(new string[]{folderPath, appName, fileName});
 
-			if (!File.Exists (path))
-				using (File.Create(path));
+			if (!Directory.Exists(Path.GetDirectoryName(filePath)))
+				Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+
+			if (!File.Exists(filePath))
+			{
+				using (FileStream fs = File.Create(filePath))
+				{
+					GetDefaultConfDoc().Save(fs);
+
+					using (StreamWriter sw = new StreamWriter(fs))
+						sw.WriteLine(); // HACK: add an empty line at the end of the file
+				}
+			}
+
+			confDoc = XDocument.Load(filePath);
 		}
+
+		#region LEGACY
+		private string oldPath;
 
 		public string[] GetSymbols()
 		{
 			List<string> symbols = new List<string>();
 			string line;
 
-			using (StreamReader reader = new StreamReader(path)) 
+			using (StreamReader reader = new StreamReader(oldPath)) 
 			{
 			    while ((line = reader.ReadLine()) != null) 
 			        symbols.Add(line);
@@ -37,12 +59,30 @@ namespace indicatorstocks
 
 			return symbols.ToArray();
 		}
+		#endregion
 
-		public void SetSymbols(string[] symbols)
+//		public void SetSymbols(string[] symbols)
+//		{
+//
+//		}
+
+		public int UpdateInterval
 		{
+			get {return Int32.Parse(confDoc.Root.Element("Settings").Element("UpdateInterval").Value);}
 
+			set {confDoc.Root.Element("Settings").Element("UpdateInterval").Value = String.Format("{0}", value);}
 		}
 
+		private XDocument GetDefaultConfDoc()
+		{
+			return new XDocument(
+				new XElement("Configuration",
+					new XElement("Stocks"),
+					new XElement("Settings",
+			             new XElement("UpdateInterval", 30))
+			    )
+			);
+		}
 
 	}
 }
